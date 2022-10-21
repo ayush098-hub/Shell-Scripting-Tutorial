125cad7b27a882b6a001177897e7a26e546f2c1b
Ayush Kumar <vermaayush99@gmail.com>
2022-10-21 20:16:28 +0530

    first commit

---
+++ 02_varibales.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Varibles : container which will hold some values."
+
+echo "User Defined Variables"
+
+name="Ayush"
+age=20
+pointer=9.89
+
+echo $name $age
+
+
+echo "Environment Variables"
+
+NOW=$( date '+%F_%H:%M:%S' )
+echo "First envnt variable: " $NOW
+echo "Second envnt variable: " $USER
+echo "Third envnt variable: " $SHELL
---
+++ 03_readinput.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Read input from user"
+
+echo "Enter your name please: "
+
+read name
+
+echo "Your name is: " $name
+
+read -p "Enter your age: " age
+
+echo "Your age is: " $age
+
+read -p "Username: " Username
+read -sp "Password:" Password
+echo ""
+
+echo "Username: " $Username "Password" $Password
+
